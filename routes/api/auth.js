e1d6fb7a00ef8521c0424f9524ea8d5cb2e9fe4d
nyarunda <ezranyagaka96@gmail.com>
2020-06-23 16:40:59 +0300

    Create profile route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bycrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+
 const User = require('../../models/User');
 
 // @route GET api/auth
@@ -15,5 +20,61 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route GET api/auth
+// @desc Authorizing user and token
+// @access Public
+router.post(
+  '/',
+  [
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Please is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check usr existence
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bycrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => {
-  res.send('Profile router');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route GET api/profile/me
+// @desc Get current user profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({
+        msg: 'Profile does not exisist',
+      });
+    }
+    res.send(profile);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Server error');
+  }
 });
 
 module.exports = router;
